593709473a648bc1a45cb66752209c0cd1dabf56
Ashrith Yerrapragada <ashrith.maudgalya@gmail.com>
2021-01-16 14:52:18 +0000

    Separated DisplayResult component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Box, Grid, Grommet, Heading, Text, Image, Button } from "grommet";
 import { Card, CardHeader, CardBody, CardFooter } from "grommet";
 import { FaGithub } from "react-icons/fa";
 import { grommet } from "grommet/themes";
+import { DisplayResult } from "./components/DisplayResult";
 
 function App() {
   return (
@@ -39,60 +40,7 @@ function App() {
               image: "url(images/bg.svg)",
             }}
           >
-            <Card height="50%" width="large" background="light-1">
-              <CardHeader pad={{ horizontal: "medium" }}>
-                <Heading level="1" style={{ fontWeight: "bold" }}>
-                  WhatPlane?
-                </Heading>
-                <Button
-                  size="small"
-                  color="light-3"
-                  primary
-                  // label="GitHub"
-                  icon={<FaGithub />}
-                />
-              </CardHeader>
-
-              <CardBody pad="small">
-                <Box>
-                  <Image fit="contain" src="images/a350_1.jpg" />
-                </Box>
-              </CardBody>
-              <CardFooter
-                pad={{ horizontal: "medium", vertical: "small" }}
-                background="#F2F2F2"
-              >
-                <Text
-                  size="small"
-                  color="brand"
-                  weight="bold"
-                  style={{ textTransform: "uppercase", color: "#63B3ED" }}
-                >
-                  Prediction
-                </Text>
-                <Text
-                  weight="bold"
-                  size="medium"
-                  style={{ textTransform: "uppercase" }}
-                >
-                  Class Name
-                </Text>
-                <Box
-                  background="status-ok"
-                  pad={{ horizontal: "xsmall", vertical: "xsmall" }}
-                  round="xsmall"
-                >
-                  <Text
-                    size="small"
-                    weight="bold"
-                    color="white"
-                    style={{ textTransform: "uppercase" }}
-                  >
-                    90 Percent
-                  </Text>
-                </Box>
-              </CardFooter>
-            </Card>
+            <DisplayResult />
           </Box>
         </Grid>
       </Box>
