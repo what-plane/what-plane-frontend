d999e82c897134388e6eb9836ec5e4aa39f4d5fa
Will Parr <parr.will.08@gmail.com>
2021-01-30 17:37:38 +0000

    Updated App.tsx to use new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,85 @@
 import React from "react";
-import { Box, Grid, Grommet, Heading, Text, Image, Button } from "grommet";
-import { Card, CardHeader, CardBody, CardFooter } from "grommet";
+import {
+  Grommet,
+  Box,
+  Header,
+  Heading,
+  Button,
+} from "grommet";
 import { FaGithub } from "react-icons/fa";
 import { grommet } from "grommet/themes";
-import { DisplayResult } from "./components/DisplayResult";
+import { MainContent } from "./components/MainContent";
+import styled from "styled-components";
 
 function App() {
   return (
     <Grommet theme={grommet} full>
-      <Box height="100%">
-        <Grid
-          fill
-          rows={["auto", "flex"]}
-          columns={["auto", "flex"]}
-          areas={[
-            { name: "header", start: [0, 0], end: [1, 0] },
-            { name: "main", start: [1, 1], end: [1, 1] },
-          ]}
+      <PageWrapper className="PageWrapper" fill="vertical" overflow="auto" align="center" flex="grow">
+        <Header
+          align="center"
+          direction="row"
+          flex={false}
+          justify="between"
+          gap="medium"
+          pad="small"
+          fill="horizontal"
+          background={{ color: "brand" }}
+        />
+        <Box
+          className="PageMain"
+          align="center"
+          justify="start"
+          fill="vertical"
+          width="large"
+          pad={{ horizontal: "xlarge" }}
         >
-          <Box
-            gridArea="header"
-            direction="row"
-            align="center"
-            justify="between"
-            style={{ backgroundColor: "#ED64A6" }}
-            pad={{ left: "medium", right: "small", vertical: "small" }}
-          />
-          <Box
-            gridArea="main"
-            pad="medium"
-            responsive={true}
-            align="center"
-            justify="start"
-            direction="column"
-            background={{
-              position: "50% 70%",
-              repeat: "no-repeat",
-              size: "contain",
-              image: "url(images/bg.svg)",
-            }}
-          >
-            <DisplayResult />
+          <Box className="BackgroundImage" width="100%" height="100%"></Box>
+          <Box className="PageContent">
+            <Box
+              align="center"
+              justify="between"
+              direction="row-responsive"
+              fill="horizontal"
+            >
+              <Heading>WhatPlane?</Heading>
+              <Button
+                size="medium"
+                href="https://github.com/what-plane"
+                target="_blank"
+                icon={<FaGithub />}
+              />
+            </Box>
+            <MainContent />
           </Box>
-        </Grid>
-      </Box>
+        </Box>
+      </PageWrapper>
     </Grommet>
   );
 }
 
 export default App;
+
+const PageWrapper = styled(Box)`
+  .BackgroundImage {
+    position: absolute;
+    z-index: 0;
+    opacity: 0.4;
+    background-position: 50% 100%;
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-image: url(images/Airport.svg);
+    @media (min-height: 1000px) {
+      opacity: 1;
+      background-position: 50% 100%;
+    }
+  }
+  .PageMain {
+    position: relative;
+    min-height: 550px;
+    min-width: 800px;
+  }
+  .PageContent {
+    position: relative;
+    z-index: 1;
+  }
+`;
