94bf4488a9a39a885336edefaf8a0a8de7781681
Ashrith Yerrapragada <ashrith.maudgalya@gmail.com>
2021-02-03 20:36:42 +0000

    Working Azure upload and prediction API

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Heading, Box } from "grommet";
 import { ImageUpload } from "./ImageUpload";
-import { LoadingSpinner } from "./LoadingSpinner"
+import { LoadingSpinner } from "./LoadingSpinner";
 import { DisplayResults } from "./DisplayResults";
 
+import uploadFileToBlob from "../services/storage";
+import fetchPrediction from "../services/predictions";
 
 export const MainContent = () => {
+  const [imageURL, setImageURL] = useState<string>("");
+  const [prediction, setPrediction] = useState({
+    class_name: "",
+    class_prob: "0",
+  });
+
+  // file to upload to container
+  const [fileSelected, setFileSelected] = useState<File>();
+
+  const [uploading, setUploading] = useState(false);
+
+  const onFileSelect = (selectedFiles: Array<File>) => {
+    // capture file into state
+    setFileSelected(selectedFiles[0]);
+  };
+
+  const onClickSubmit = async () => {
+    if (fileSelected) {
+      if (!["image/jpeg", "image/png"].includes(fileSelected?.type)) {
+        alert("Please select an image filetype (jpg/png)");
+      } else if (fileSelected.size > 20971520) {
+        alert("Please upload a smaller image (max file size: 20Mb)");
+      } else {
+        setUploading(true);
+        // Upload to Azure Storage
+        const thisBlobURL: string = await uploadFileToBlob(fileSelected);
+
+        // prepare UI for results
+        setImageURL(thisBlobURL);
+        const predObject = await fetchPrediction(thisBlobURL);
+        setPrediction(predObject);
+
+        // reset state
+        setFileSelected(undefined);
+        setUploading(false);
+      }
+    }
+  };
   return (
     <Box
       align="center"
@@ -31,7 +71,7 @@ export const MainContent = () => {
           Upload Image
         </Heading>
       </Box>
-      <ImageUpload />
+      <ImageUpload onFileSelect={onFileSelect} onClickSubmit={onClickSubmit} />
     </Box>
   );
 };
