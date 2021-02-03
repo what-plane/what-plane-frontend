2b0d79de3c9ba5a6cc38c9ac9a88d8f91cf54bcb
Ashrith Yerrapragada <ashrith.maudgalya@gmail.com>
2021-02-03 20:36:22 +0000

    Updated ImageUpload.tsx to upload to Azure

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -2,28 +2,39 @@ import React from "react";
 import { Button, Form, Box, Text } from "grommet";
 import Dropzone from "react-dropzone";
 
-export const ImageUpload = () => {
+export const ImageUpload = ({
+  onFileSelect,
+  onClickSubmit,
+}: {
+  onFileSelect: (event: any) => void;
+  onClickSubmit: () => Promise<void>;
+}) => {
   return (
     <Box>
       <Form>
         <Box align="center" justify="center" fill="horizontal" pad="xsmall">
-          <Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+          <Dropzone
+            onDrop={onFileSelect}
+            accept={["image/jpeg", "image/png"]}
+            minSize={0}
+            maxSize={20971520}
+          >
             {({ getRootProps, getInputProps }) => (
-                <div {...getRootProps()}>
-                  <input {...getInputProps()} />
-                  <Box
-                    align="center"
-                    justify="center"
-                    background={{ color: "dark-5" }}
-                    round="small"
-                    pad="medium"
-                    fill="horizontal"
-                  >
-                    <Text color="white">
-                      Drag and drop an image here, or click to select
-                    </Text>
-                  </Box>
-                </div>
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <Box
+                  align="center"
+                  justify="center"
+                  background={{ color: "dark-5" }}
+                  round="small"
+                  pad="medium"
+                  fill="horizontal"
+                >
+                  <Text color="white">
+                    Drag and drop an image here, or click to select
+                  </Text>
+                </Box>
+              </div>
             )}
           </Dropzone>
         </Box>
@@ -34,7 +45,7 @@ export const ImageUpload = () => {
           direction="row-responsive"
           fill="horizontal"
         >
-          <Button label="Submit" />
+          <Button label="Submit" onClick={onClickSubmit} />
         </Box>
       </Form>
     </Box>
