440e81ee5810150840e3c75477617026d815d910
Will Parr <parr.will.08@gmail.com>
2021-02-04 21:38:12 +0000

    Include typing and calculate class prob and class status

--- src/services/predictions.ts
+++ src/services/predictions.ts
@@ -1,21 +1,51 @@
 import axios from "axios";
 
-const filenameRegex: string = "(?<=/)[^/?#]+(?=[^/]*$)";
+const uuidRegex: string = "(?<=/)[^/?#]+(?=[^/]*$)";
+export interface PredictData {
+  predictions: Array<Prediction>;
+  predictor: string;
+  topk: string;
+}
 
-const fetchPrediction = async (blobURL: string) => {
-  console.log(blobURL);
-  const filename = blobURL.match(filenameRegex) || [];
-  const data = await axios
-    .get(`https://whatplane-docker.azurewebsites.net/predict/${filename}`)
+export interface Prediction {
+  class_name: string;
+  class_prob: number;
+  class_status?: string;
+}
+
+export const fetchPrediction = async (blobURL: string) => {
+  const uuid = blobURL.match(uuidRegex);
+  let data: PredictData = await axios
+    .get<PredictData>(
+      `https://whatplane-docker.azurewebsites.net/predict/${uuid}`
+    )
     .then((response) => {
       return response.data;
     });
 
-  console.log(data);
-  return {
-    class_name: data.predictions[0].class_name,
-    class_prob: data.predictions[0].class_prob,
+  data = {
+    ...data,
+    predictions: data.predictions.map<Prediction>((pred: Prediction) => {
+      return <Prediction>{
+        ...pred,
+        class_prob: pred.class_prob * 100,
+        class_status: calcClassStatus(pred.class_prob),
+      };
+    }),
   };
+
+  return data.predictions[0];
 };
 
-export default fetchPrediction;
+const calcClassStatus = (classProb: number) => {
+  let predictionStatus: String;
+
+  if (classProb > 0.8) {
+    predictionStatus = "status-ok";
+  } else if (classProb > 0.6) {
+    predictionStatus = "status-warning";
+  } else {
+    predictionStatus = "status-error";
+  }
+  return predictionStatus;
+};
