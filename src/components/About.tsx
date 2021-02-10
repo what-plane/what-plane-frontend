4fce81ed40ceb87afcf43e1c768168b3fc39becb
Will Parr <parr.will.08@gmail.com>
2021-02-10 23:25:29 +0000

    Use our theme rather than grommet default

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,11 +10,12 @@ import {
   Paragraph,
 } from "grommet";
 import { deepMerge } from "grommet/utils";
-import { grommet, ThemeType } from "grommet/themes";
+import { ThemeType } from "grommet/themes";
 import { Close } from "grommet-icons";
+import { WhatPlaneTheme } from "../theme/WhatPlane";
 import fetchClasses from "../services/classes";
 
-const theme: ThemeType = deepMerge(grommet, {
+const theme: ThemeType = deepMerge(WhatPlaneTheme, {
   list: {
     item: {
       pad: { horizontal: "large", vertical: "xsmall" },
