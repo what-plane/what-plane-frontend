2721c2bec39fe0c95af7da8fbe02a02ebc783318
Ashrith Yerrapragada <ashrith.maudgalya@gmail.com>
2021-02-10 20:29:43 +0000

    Updated About component to list the classes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,17 +1,46 @@
-import React from "react";
-import { Box, Button, Grommet, Heading, Layer, Text } from "grommet";
-import { WhatPlaneTheme } from "../theme/WhatPlane";
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Grommet,
+  Heading,
+  Layer,
+  Text,
+  List,
+  Paragraph,
+} from "grommet";
+import { deepMerge } from "grommet/utils";
+import { grommet, ThemeType } from "grommet/themes";
 import { Close } from "grommet-icons";
+import fetchClasses from "../services/classes";
+
+const theme: ThemeType = deepMerge(grommet, {
+  list: {
+    item: {
+      pad: { horizontal: "large", vertical: "xsmall" },
+      background: ["white", "light-2"],
+      border: true,
+    },
+  },
+});
 
 export const About = () => {
-  const [open, setOpen] = React.useState<boolean>();
+  const [open, setOpen] = useState<boolean>();
+  const [predClassNames, setPredClassNames] = useState<Array<string>>([]);
 
-  const onOpen = () => setOpen(true);
+  const onOpen = async () => {
+    const classes = await fetchClasses();
+    setPredClassNames(classes.class_names);
+    setOpen(true);
+  };
 
-  const onClose = () => setOpen(undefined);
+  const onClose = async () => {
+    setOpen(undefined);
+    setPredClassNames([]);
+  };
 
   return (
-    <Grommet theme={WhatPlaneTheme}>
+    <Grommet theme={theme}>
       <Box>
         <Button
           label={
@@ -29,7 +58,22 @@ export const About = () => {
             <Heading level={3} margin="none">
               About
             </Heading>
-            <Text>About page</Text>
+            <Paragraph size="small">
+              WhatPlane uses Deep Learning to detect the types of aircraft in
+              images. It also uses the ImageNet model to identify images that do
+              not have an aircraft in them. <br />
+              The model currently recognises the following aircraft types, but
+              we're working hard to add more and improve the performance!
+            </Paragraph>
+            {/* <Box width="90%" align="center" justify="center"> */}
+            <Text size="xsmall" textAlign="center">
+              <Text size="small" weight="bold">
+                List of Aircraft
+              </Text>
+              <List data={predClassNames} margin="xsmall" pad="xsmall"></List>
+            </Text>
+            {/* </Box> */}
+
             <Box
               as="footer"
               gap="small"
